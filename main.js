6db669bf41aeb852f803f75d5e30a89e0dbcdd8b
Muhamad Yuga Hidayat <yugaadvance@gmail.com>
2025-08-03 00:36:12 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 alert("Website ini masih dalam pengembangan")
+
+let menuIcon = document.getElementById("pmenu-icon");
+let navbar = document.querySelector(".pnavbar");
+
+menuIcon.onclick = function () {
+    navbar.classList.toggle("active");
+};
