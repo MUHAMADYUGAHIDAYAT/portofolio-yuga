65c914a4383eacece23090d7ecb4ae1d6c6b3ea8
Muhamad Yuga Hidayat <yugaadvance@gmail.com>
2025-08-04 21:32:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ let navbar = document.querySelector(".pnavbar");
 menuIcon.onclick = function () {
     navbar.classList.toggle("active");
 };
+
+let pesan = document.getElementById("lagi-lesu");
+pesan.onclick = function () {
+    alert("Fitur Masih Belum Tersedia");
+};
+
+let pengalaman = document.querySelector(".ppengalaman");
+pengalaman.onclick = function () {
+    alert("Fitur Masih Belum DItambah")
+}
