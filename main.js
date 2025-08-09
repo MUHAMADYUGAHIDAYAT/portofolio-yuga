221a56e3ae8684e82337d51e23d402a988202ce1
Muhamad Yuga Hidayat <yugaadvance@gmail.com>
2025-08-09 15:48:05 +0700

    Update main.js

--- main.js
+++ main.js
@@ -8,9 +8,22 @@ menuIcon.onclick = function () {
 };
 
 let pesan = document.getElementById("raffiayam");
+let popupwa = document.getElementById("ke-wa-bae");
+let tombolIyo = document.getElementById("btn-iyo");
+let tombolIdak = document.getElementById("btn-idak");
+
 pesan.onclick = function () {
-    alert("Fitur Masih Belum Tersedia");
+    alert("fitur masih belum tersedia");
+    popupwa.classList.add("active");
 };
+tombolIyo.onclick = function () {
+    window.location.href = "http://wa.me/6287780313222"
+    popupwa.classList.remove("active");
+}
+tombolIdak.onclick = function () {
+    popupwa.classList.remove("active");
+}
+
 
 let pengalaman = document.querySelector(".ppengalaman");
 pengalaman.onclick = function () {
